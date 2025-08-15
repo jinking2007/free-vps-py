ff97a9f8a2ba7dea35426535fc84fe6e0bb879f0
KKKLEIN <31945072+jinking2007@users.noreply.github.com>
2025-08-15 23:27:23 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -511,7 +511,7 @@ new_config = '''config = {
                 {
                     "type": "field",
                     "domain": [
-                        "youtube.com", "youtu.be",
+                        "youtube.com", "youtu.be", "instagram.com", "discord.com", "facebook.com",
                         "googlevideo.com",
                         "ytimg.com",
                         "gstatic.com",
